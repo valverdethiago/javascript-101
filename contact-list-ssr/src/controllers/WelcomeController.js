76bb57ee810e5eb353e5723e6e3630f0752e6822
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2020-07-01 19:08:02 -0300

    Adding controllers to contact list app

--- contact-list-ssr/src/controllers/WelcomeController.js
+++
@@ -1,4 +0,0 @@
-exports.welcomeController = (req, res) => {
-    console.log('Entrou no post');
-    res.render('welcome', req.body);
-}
--- contact-list-ssr/src/routes.js
+++ contact-list-ssr/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./controllers/HomeController');
-const welcomeController = require('./controllers/WelcomeController');
+const loginController = require('./controllers/LoginController');
+const registerController = require('./controllers/RegisterController');
 
-route.get('/', homeController.landingPage);
-route.post('/post', welcomeController.welcomeController);
+route.get('/', homeController.index);
+route.get('/login', loginController.index);
+route.get('/register', registerController.index)
 
 module.exports = route;
